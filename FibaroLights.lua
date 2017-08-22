b70758b7418c1ddef590f4acd8ab69a5061507ac
I3udder <jeroen.vercoulen@gmail.com>
2017-08-22 19:10:57 +0200

    addition of description

--- FibaroLights.lua
+++ FibaroLights.lua
@@ -3,3 +3,8 @@
 %% properties
 %% globals
 --]]
+
+-- Script to handle automatic lights on/off switch
+-- Jeroen Vercoulen jeroen.vercoulen@gmail.com
+
+-- This script requires a global variable name 'LightStatus' with values 0 and 1
