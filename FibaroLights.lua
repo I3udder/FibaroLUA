3b98db619cf0aaa0bea986fc0b820dda7928c9ed
I3udder <jeroen.vercoulen@gmail.com>
2017-08-22 19:35:42 +0200

    Completed the Scene
    Test phase if the scene works as intended

--- FibaroLights.lua
+++ FibaroLights.lua
@@ -8,3 +8,48 @@
 -- Jeroen Vercoulen jeroen.vercoulen@gmail.com
 
 -- This script requires a global variable name 'LightStatus' with values 0 and 1
+
+local sourceTrigger = fibaro:getSourceTrigger();
+function PowerOff()
+local currentDate = os.date("*t");
+local startSource = fibaro:getSourceTrigger();
+if (
+ ( string.format("%02d:%02d", currentDate.hour, currentDate.min) == "23:59" ) -- You can specify any time to poweroff the lights
+)
+then
+if (tonumber(LightStatus) == 1)
+then
+  -- disable continuously poweroff devices
+  fibaro:setGlobal("LightStatus",0)
+  -- You can specify as much lights you want. Just copy the line below.
+  fibaro:call(6, "turnOff");
+  fibaro:call(101, "turnOff");
+ end
+ end
+ if (
+ ( string.format("%02d:%02d", currentDate.hour, currentDate.min) == "21:00" ) -- You can specify any time to poweroff the lights
+)
+then
+  -- You can specify as much lights you want. Just copy the line below.
+  fibaro:call(8, "turnOff");
+end
+
+ end
+
+if (sourceTrigger["type"] == "autostart") then
+PowerOff()
+else
+
+local currentDate = os.date("*t");
+local startSource = fibaro:getSourceTrigger();
+if (
+startSource["type"] == "other"
+)
+then
+	fibaro:call(6, "turnOff");
+	fibaro:call(8, "turnOff");
+	fibaro:call(101, "turnOff");
+end
+
+end
+ 
